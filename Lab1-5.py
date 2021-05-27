99e65573c51c60079eee1c4c2c4154a0fb5700f7
JM O'Brien <83389601+jmobriencs@users.noreply.github.com>
2021-05-27 02:05:00 -0700

    Add files via upload

---
+++ Lab1-5.py
@@ -0,0 +1,13 @@
+#John-Michael O'Brien
+#w1890922
+#CISP 300
+#2/1/20
+#This program calculates the average number of wins over five years.
+
+year1 = int(input('Enter the number of wins for year 1: '))
+year2 = int(input('Enter the number of wins for year 2: '))
+year3 = int(input('Enter the number of wins for year 3: '))
+year4 = int(input('Enter the number of wins for year 4: '))
+year5 = int(input('Enter the number of wins for year 5: '))
+averageWins = (year1 + year2 + year3 + year4 + year5)/5
+print ('The average number of wins over a five-year period: ', averageWins)
